84dc0a21cf7c478889cd57033db83dec2732eff1
Samuel Yin <wsgddjy@live.com>
2025-05-19 11:57:59 +0800

    first commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3B82F6",
+        secondary: "#1E40AF",
+      },
+      animation: {
+        fadeIn: "fadeIn 1s ease-in",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
